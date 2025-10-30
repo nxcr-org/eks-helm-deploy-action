a304add9a8c556b8fbe4b95ba7a620ccf0feee79
grante-nxcr <102606801+grante-nxcr@users.noreply.github.com>
2025-10-30 09:19:26 -0500

    removing manually adding aws cli

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
-
 # Login to Kubernetes Cluster.
 if [ -n "$CLUSTER_ROLE_ARN" ]; then
     aws eks \
