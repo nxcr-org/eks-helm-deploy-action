7c4a59af400401da7ef8503a44a10a7957b80397
grante-nxcr <102606801+grante-nxcr@users.noreply.github.com>
2025-11-07 12:51:53 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -44,7 +44,7 @@ fi
 # Helm upgrade
 ####################
 
-UPGRADE_COMMAND="helm upgrade -i --timeout ${TIMEOUT}"
+UPGRADE_COMMAND="helm upgrade --install -i --timeout ${TIMEOUT}"
 for config_file in ${DEPLOY_CONFIG_FILES//,/ }
 do
     UPGRADE_COMMAND="${UPGRADE_COMMAND} -f ${config_file}"
